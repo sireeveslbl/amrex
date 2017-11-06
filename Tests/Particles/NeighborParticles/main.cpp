259000ed5962108f064fce45a25a689ee0f2505c
Andrew Myers <atmyers2@gmail.com>
2017-11-06 12:08:46 -0800

    update neighbor test

--- Tests/Particles/NeighborParticles/main.cpp
+++ Tests/Particles/NeighborParticles/main.cpp
@@ -33,25 +33,36 @@ class MyParticleContainer
 
     void printNL() {
         const int lev = 0;
-
-#ifdef _OPENMP
-#pragma omp parallel
-#endif
         for (MyParIter pti(*this, lev, MFItInfo().SetDynamic(false)); pti.isValid(); ++pti) {
             PairIndex index(pti.index(), pti.LocalTileIndex());
-            int size = neighbor_list[index].size();
-            for (int j = 0; j < size; ++j) {
-                amrex::AllPrintToFile("nl") << neighbor_list[index][j] << " ";
+            const auto& nl = neighbor_list[index];
+            const int size = nl.size();
+            // if (pti.index() == 3 and pti.LocalTileIndex() == 511) {
+            //     int ind = 0;
+            //     while (ind < size) {
+            //         amrex::AllPrintToFile("out") << nl[ind] << "\n" << "\t";                    
+                    
+            //         for (int i = ind+1; i < ind + 3*nl[ind]+1; ++i) {
+            //             amrex::AllPrintToFile("out") << nl[i] << " ";
+            //         }
+            //         amrex::AllPrintToFile("out") << "\n";                    
+            //         ind += 3*nl[ind] + 1;
+            //     }
+            // }
+            int ind = 0;
+            while (ind < size) {
+                amrex::AllPrintToFile("nl") << "(" << pti.index() << ", " << pti.LocalTileIndex() << "): " << nl[ind] << std::endl;
+                ind += 3*nl[ind] + 1;
             }
-            amrex::AllPrintToFile("nl") << std::endl;
         }
     }
-
+    
 private:
 
     inline virtual bool check_pair(const ParticleType& p1, const ParticleType& p2) final {
-        return (std::abs(p1.pos(0) - p2.pos(0)) < 0.046875) and 
-               (std::abs(p1.pos(1) - p2.pos(1)) < 0.046875);
+        return (std::abs(p1.pos(0) - p2.pos(0)) < 0.01171875) and
+               (std::abs(p1.pos(1) - p2.pos(1)) < 0.01171875) and
+               (std::abs(p1.pos(2) - p2.pos(2)) < 0.01171875);
     }
 };
 
@@ -71,7 +82,7 @@ void test_neighbor_list(TestParams& parms)
   // This sets the boundary conditions to be doubly or triply periodic
   int is_per[BL_SPACEDIM];
   for (int i = 0; i < BL_SPACEDIM; i++) 
-    is_per[i] = 1;
+    is_per[i] = 0;
   Geometry geom(domain, &real_box, CoordSys::cartesian, is_per);
 
   BoxArray ba(domain);
@@ -85,16 +96,16 @@ void test_neighbor_list(TestParams& parms)
 
   MyParticleContainer::ParticleInitData pdata = {10.0};
   myPC.InitOnePerCell(0.5, 0.5, 0.5, pdata);
+
   myPC.Redistribute();
+  myPC.fillNeighbors(0);
+  myPC.buildNeighborList(0);
 
-//  myPC.fillNeighbors(0);
-//  myPC.buildNeighborList(0);
+  for (int i = 0; i < 25; ++i) {
+      myPC.updateNeighbors(0);
+  }
 
-//  for (int i = 0; i < 25; ++i) {
-//      myPC.updateNeighbors(0);
-//  }
-
-  myPC.printNL();
+  //  myPC.printNL();
 
 }
 
