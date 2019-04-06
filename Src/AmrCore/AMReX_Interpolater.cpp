df2626d5e34e003eb508c7761ffa1fbf0e565c13
sireeveslbl <sireeves@ucsc.edu>
2019-04-05 19:25:57 -0700

    CHanges to merge development for AMReX

--- Src/AmrCore/AMReX_Interpolater.cpp
+++ Src/AmrCore/AMReX_Interpolater.cpp
@@ -858,7 +858,7 @@ CellGaussianProcess::interp (const FArrayBox& crse,
                              const IntVect&   ratio,
                              const Geometry&  crse_geom,
                              const Geometry&  fine_geom,
-                             Vector<BCRec>&    bcr,
+                             Vector<BCRec> const&    bcr,
                              int              actual_comp,
                              int              actual_state)
 {
